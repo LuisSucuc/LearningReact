e8ca1d97b82fe91da7790e28d66bc9e65630e26d
Luis Sucuc <luissucuco@gmail.com>
2021-02-16 14:29:57 -0600

    Simple examples

--- src/index.js
+++ src/index.js
@@ -1,19 +1,36 @@
 import React from "react";
 import ReactDom from "react-dom";
 
+/*
 function Greeting() {
   return (
     <React.Fragment>
-      <article>
+      <div className="my-class">
         <h3>Hello world</h3>
         <ul>
           <li>
             <a href="#">Hi</a>
           </li>
+          <img src="" alt="" />
+          <input type="text" />
         </ul>
-      </article>
+      </div>
     </React.Fragment>
   );
+}*/
+
+function Greeting() {
+  return (
+    <div>
+      <Person />
+      <Message />
+    </div>
+  );
 }
+
+const Person = () => <h2>Luis Sucuc</h2>;
+const Message = () => {
+  return <p>Message</p>;
+};
 
 ReactDom.render(<Greeting />, document.getElementById("root"));
