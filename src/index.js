7bd365ff4ea4668f26b1b90a6d5dc38a5e347cc1
Luis Sucuc <luissucuco@gmail.com>
2021-02-16 16:38:24 -0600

    props

--- src/index.js
+++ src/index.js
@@ -3,49 +3,41 @@ import ReactDom from "react-dom";
 
 //CSS
 import "./index.css";
+/*Options
+<h4 style={{ color: "#617d98", fontSize: "0.75rem", margin: "0.25rem" }}>
+        Matthew Sucuc
+      </h4>
+*/
+
+const books = {
+  title: "Hola",
+  author: "man",
+  img: "https://eloquentjavascript.net/img/cover.jpg",
+};
 
 function BookList() {
   return (
     <section className="bookList">
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
+      <Book img={books.img} author={books.author} title={books.title}>
+        <p>Lorem ipsum dolor sit amet</p>
+      </Book>
+      <Book img={books.img} author={books.author} title={books.title}>
+        <p>Lorem ipsum dolor sit amet</p>
+      </Book>
     </section>
   );
 }
 
-const Book = () => {
+const Book = ({ img, title, author, children }) => {
+  //const { img, title, author } = props;
   return (
     <article className="book">
-      <Image></Image>
-      <Title />
-      <Author />
+      <img src={img} alt="Book" width="100%" />
+      <h1>{title}</h1>
+      <h4>{author.toUpperCase()}</h4>
+      {children}
     </article>
   );
 };
-
-const Image = () => (
-  <img
-    src="https://eloquentjavascript.net/img/cover.jpg"
-    alt="Book"
-    width="100%"
-  />
-);
-
-const Title = () => <h1>Title</h1>;
-
-const Author = () => (
-  <h4 style={{ color: "#617d98", fontSize: "0.75rem", margin: "0.25rem" }}>
-    Matthew Sucuc
-  </h4>
-);
 
 ReactDom.render(<BookList />, document.getElementById("root"));
