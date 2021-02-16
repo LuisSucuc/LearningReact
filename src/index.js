fe6e1304b47876ae7a37f14e6a77c65dfcda2261
Luis Sucuc <luissucuco@gmail.com>
2021-02-16 15:27:35 -0600

    Css directly

--- src/index.js
+++ src/index.js
@@ -36,12 +36,16 @@ const Image = () => (
   <img
     src="https://eloquentjavascript.net/img/cover.jpg"
     alt="Book"
-    width="150px"
+    width="100%"
   />
 );
 
 const Title = () => <h1>Title</h1>;
 
-const Author = () => <h4>Matthew Sucuc</h4>;
+const Author = () => (
+  <h4 style={{ color: "#617d98", fontSize: "0.75rem", margin: "0.25rem" }}>
+    Matthew Sucuc
+  </h4>
+);
 
 ReactDom.render(<BookList />, document.getElementById("root"));
